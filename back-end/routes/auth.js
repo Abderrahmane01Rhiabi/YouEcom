c96e46e36b10db87f1aa6812e8df06c459e52b85
Abdou RH <rhiabi.abderrahmane@gmail.com>
2021-05-29 17:47:12 +0100

    11 commit

--- back-end/routes/auth.js
+++ back-end/routes/auth.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { registerUser, loginUser, logout, forgotPass } = require('../controllers/authController');
+const { registerUser, loginUser, logout, forgotPass, resetPass } = require('../controllers/authController');
 
 router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
 router.route('/logout').get(logout);
 router.route('/password/forgot').post(forgotPass);
+router.route('/password/reset/:token').post(resetPass);
+
+
 
 module.exports = router; 
