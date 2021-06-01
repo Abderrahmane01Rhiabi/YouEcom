20bdbd379f8796f19ec41bc201ab15216981622f
Abdou RH <rhiabi.abderrahmane@gmail.com>
2021-06-01 20:52:24 +0100

    16 commit

--- back-end/controllers/orderController.js
+++ back-end/controllers/orderController.js
@@ -46,7 +46,7 @@ exports.myOrders = catchAsyncErrors( async (req,res,next) =>{
     
 })
 
-//get all orders => /api/v1/admin/orders/
+//get all orders => /api/v1/admin/orders
 exports.allOrders = catchAsyncErrors( async (req,res,next) =>{
   
     const orders = await Order.find();
@@ -63,3 +63,35 @@ exports.allOrders = catchAsyncErrors( async (req,res,next) =>{
     })
     
 })
+
+//update process order => /api/v1/admin/order/:id
+exports.updateOrder = catchAsyncErrors( async (req,res,next) =>{
+  
+    const order = await Order.findById(req.params.id);
+
+    if(order.orderStatus === 'Delivered'){
+        return next(new ErrorHandler('You have already delivered this order', 400));
+    }
+
+    order.orderItems.forEach(async item =>{
+        await updateStock(item.product, item.quantity)
+    })
+
+    order.orderStatus = req.body.status;
+    order.deliveredAt = Date.now();
+    
+    await order.save();
+
+    res.status(200).json({
+        success : true
+    })
+    
+})
+
+async function updateStock(id, quantity){
+    const product = await Product.findById(id);
+
+    product.stock = product.stock - quantity;
+
+    await product.save({ validateBeforeSave : false});
+}
--- back-end/routes/order.js
+++ back-end/routes/order.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { newOrder, myOrders, getSingleOrder, allOrders } = require('../controllers/orderController');
+const { newOrder, myOrders, getSingleOrder, allOrders, updateOrder } = require('../controllers/orderController');
 
 const { isAuthUser, authRoles } = require('../middlewares/auth');
  
@@ -9,5 +9,8 @@ router.route('/order/:id').get(isAuthUser,getSingleOrder);
 router.route('/orders/me').get(isAuthUser,myOrders);
 
 router.route('/admin/orders/').get(isAuthUser, authRoles('admin'), allOrders);
+router.route('/admin/order/:id').put(isAuthUser, authRoles('admin'), updateOrder);
+
 
 module.exports = router;
+ 
